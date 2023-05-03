07e6db3f8ebe0a976d023b3a9fa7b6cea219407f
Joyce Ann Miguel <joyce.a.miguel@gmail.com>
2023-05-03 19:28:30 -0400

    added count letters

---
+++ countLetters.js
@@ -0,0 +1,19 @@
+const assertEqual = function(actual,expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual}  === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !==  ${expected}`);
+  }
+};
+const countLetters = function (string){
+  const result = {};
+  for (let char of string) {
+    if (result[char] === undefined) {
+      result[char] = 1;
+    } else {
+      result[char]++;
+    }
+  } return result;
+}
+
+console.log(countLetters("lighthouse in the house"));
