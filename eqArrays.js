e9369ef2554f3e5b17d3cd951075b12b7995c960
Joyce Ann Miguel <joyce.a.miguel@gmail.com>
2023-05-07 21:15:03 -0400

    added refractoring

--- eqArrays.js
+++ eqArrays.js
@@ -1,13 +1,5 @@
-const assertEqual = function(actual,expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual}  === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !==  ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1,1);
 const eqArrays = function(array1,array2){
   if (array1.length !== array2.length){
     return false;
@@ -18,5 +10,3 @@ const eqArrays = function(array1,array2){
     }
 } return true;
 }
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2], [1, 2, 3]), false);
